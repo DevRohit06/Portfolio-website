10d0065da29e86fbdb9def07b558b2e5dd45793b
Rohit Kushwaha <technicalrohit06@gmail.com>
2023-09-15 12:26:11 +0530

    initialization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import preload from "astro-preload";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), preload(), compress()]
+});
