7cb44e3c9eb84a178d3f28859cceb6950eb31e6a
Rohit Kushwaha <technicalrohit06@gmail.com>
2023-09-17 22:47:55 +0530

    ssr with vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preload from "astro-preload";
-
 import compress from "astro-compress";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()]
+  integrations: [tailwind(), compress()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+    imageService: true,
+  })
 });
